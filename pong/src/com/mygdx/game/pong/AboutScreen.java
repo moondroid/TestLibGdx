7c46f327e3be81926c06ec26e0b96c3dbb6ebd89
Marco Granatiero <moondroidapps@gmail.com>
2014-09-26 20:17:32 +0200

    add about screen

--- pong/src/com/mygdx/game/pong/AboutScreen.java
+++ pong/src/com/mygdx/game/pong/AboutScreen.java
@@ -1,10 +1,11 @@
 package com.mygdx.game.pong;
 
 /**
- * Created by Marco on 25/09/2014.
+ * Created by Marco on 26/09/2014.
  */
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -12,62 +13,45 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
-public class MainMenuScreen implements Screen {
+public class AboutScreen implements Screen {
 
     OrthographicCamera guiCam;
     SpriteBatch batcher;
-    Rectangle newgameBounds;
-    Rectangle aboutBounds;
-    Rectangle helpBounds;
+    Rectangle backBounds;
     Vector3 touchPoint;
 
     final Game game;
 
-    public MainMenuScreen(Game game){
+    public AboutScreen(Game game){
 
         this.game = game;
 
         guiCam = new OrthographicCamera(480,320);
         guiCam.position.set(480 / 2, 320 / 2, 0);
         batcher = new SpriteBatch();
-        newgameBounds = new Rectangle(157, 125, 170, 34);
-        helpBounds = new Rectangle(157, 75, 168, 36);
-        aboutBounds = new Rectangle(157, 23, 161, 39);
+        backBounds = new Rectangle(399, 9, 66, 21);
         touchPoint = new Vector3();
-
     }
 
 
     @Override
     public void render(float delta) {
-
         GL20 gl = Gdx.gl;
         gl.glClearColor(1, 0, 0, 1);
         gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         guiCam.update();
         batcher.setProjectionMatrix(guiCam.combined);
-
         batcher.disableBlending();
         batcher.begin();
-        batcher.draw(Assets.mainMenuScreenRegion, 0, 0, 480, 320);
+        batcher.draw(Assets.aboutScreenRegion, 0, 0, 480, 320);
         batcher.end();
 
         if (Gdx.input.justTouched()){
             guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
 
-            if(OverlapTester.pointInRectangle(newgameBounds, touchPoint.x, touchPoint.y)){
+            if(OverlapTester.pointInRectangle(backBounds, touchPoint.x, touchPoint.y)){
                 Assets.playSound(Assets.bounceSound);
-                //game.setScreen(new GameScreen(game));
-                return;
-            }
-            if (OverlapTester.pointInRectangle(helpBounds, touchPoint.x, touchPoint.y)){
-                Assets.playSound(Assets.bounceSound);
-                game.setScreen(new Help1Screen(game));
-                return;
-            }
-            if (OverlapTester.pointInRectangle(aboutBounds, touchPoint.x, touchPoint.y)){
-                Assets.playSound(Assets.bounceSound);
-                game.setScreen(new AboutScreen(game));
+                game.setScreen(new MainMenuScreen(game));
                 return;
             }
         }

4fb27445c008c8743c02c2ad76c634e4f9efe31c
Marco Granatiero <moondroidapps@gmail.com>
2014-10-15 23:19:42 +0200

    disable scrolling camera y position

--- bouncingball/src/main/java/com/mygdx/game/bouncingball/OrthoCamController.java
+++ bouncingball/src/main/java/com/mygdx/game/bouncingball/OrthoCamController.java
@@ -24,7 +24,8 @@ public class OrthoCamController extends InputAdapter {
         if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
             camera.unproject(delta.set(last.x, last.y, 0));
             delta.sub(curr);
-            camera.position.add(delta.x, delta.y, 0);
+            //camera.position.add(delta.x, delta.y, 0);
+            camera.position.add(delta.x, 0, 0);
         }
         last.set(x, y, 0);
         return false;
